ef8ce7d11b164573bc722fa6b50be24dbc5f0f99
mangane <40431348+mangane@users.noreply.github.com>
2018-12-01 05:49:49 -1100

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,6 +16,11 @@ client.on('message', message => {
 
 });
 
+if (command === "prefix") {
+	guildConf[message.guild.id].prefix = args[0];
+	if (!guildConf[message.guild.id].prefix) {
+		guildConf[message.guild.id].prefix = config.prefix; // If you didn't specify a Prefix, set the Prefix to the Default Prefix
+	};
 // THIS  MUST  BE  THIS  WAY
 
 client.login(process.env.BOT_TOKEN);
