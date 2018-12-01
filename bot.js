5300e0acb93369e7cd78711bfea2ba979061b6bb
mangane <40431348+mangane@users.noreply.github.com>
2018-12-01 09:27:19 -1100

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,7 +16,9 @@ client.on('message', message => {
     
     if (message.content === ';help') { 
        
-        message.reply('voici les commandes : !ping !help');    }
+        message.reply('```voici les commandes : 
+                      !ping 
+                      !help```');    }
         
 });
 
