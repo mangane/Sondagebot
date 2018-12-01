85a969d83e1f1d61e556596fdc1b9b72871f8aa6
mangane <40431348+mangane@users.noreply.github.com>
2018-12-01 06:10:15 -1100

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,13 +10,11 @@ client.on('ready', () => {
 
 client.on('message', message => {
 
-    if (message.content === 'ping') {
+    if (message.content === ';ping') {
 
     	message.reply('pong');  	}
 
-	}
 });
-
 
 // THIS  MUST  BE  THIS  WAY
 
