d73c5d2f487b66850ce18d021f0281e0e662c5ec
mangane <40431348+mangane@users.noreply.github.com>
2018-12-01 05:56:32 -1100

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,11 +13,6 @@ client.on('message', message => {
     if (message.content === ';ping') {
 
     	message.reply('pong');  	}
-if (command === "prefix") {
-
-	guildConf[message.guild.id].prefix = args[0];	if (!guildConf[message.guild.id].prefix) {
-
-		guildConf[message.guild.id].prefix = config.prefix; // If you didn't specify a Prefix, set the Prefix to the Default Prefix
 
 	}
 });
