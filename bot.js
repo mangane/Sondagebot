cb63f73c6e722166671368f484bb342c54a96cb8
mangane <40431348+mangane@users.noreply.github.com>
2018-12-02 07:04:46 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,22 +17,10 @@ client.on('message', message => {
     if (message.content === ';help') { 
        
         message.reply('voici les commandes : `;ping` `;help`:wink:');}
-
-    if (message.content.startsWith(';sondage')) {
-    let args = message.content.split(" ").slice(1);
-    let thingToEcho = args.join(" ")
-    var embed = new Discord.RichEmbed()
-        embed.setDescription("Sondage")
-        embed.addField(thingToEcho, "Répondre avec :white_check_mark: ou :x:")
-        embed.setColor('#01A1FE')
-    message.guild.channels.find("name", "sondage").sendEmbed(embed)
-    .then(function (message){
-        message.react("✔")
-        message.react("✘")
- 
+ if (message.content === '') {
         message.reply('Hey, I\'m a reply!') 
             .then(sent => console.log(`Sent a reply to ${sent.author.username}`)) 
-            .catch(console.error);
+            .catch(console.error);}
     });
         
    
