1352e460f9cd9c46254736d091425440e480d7e8
mangane <40431348+mangane@users.noreply.github.com>
2018-12-02 07:36:08 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,17 +17,8 @@ client.on('message', message => {
     if (message.content === ';help') { 
        
         message.reply('voici les commandes : `;ping` `;help`:wink:');}
-    
-if (message.content.startsWith('!kick')) {
-const user = message.mentions.users.first();
-if (user) {
-const member = message.guild.member(user);
- if (member) {
-member.kick('Optional reason that will display in the audit logs').then(() => {
-message.reply(`Successfully kicked ${user.tag}`); }).catch(err => {
-message.reply('I was unable to kick the member');}
-});
-     
+   
+});  
         
    // THIS  MUST  BE  THIS  WAY
 
