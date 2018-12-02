5fcc67d62fdfcba127cef2bdf10c36edcc87232b
mangane <40431348+mangane@users.noreply.github.com>
2018-12-02 05:33:28 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,6 +21,38 @@ client.on('message', message => {
     if (message.content === ';say') {
         
         message.reply('en cours...');}
+if (message.content.startsWith(config.modprefix ; "kick")) {
+        if (message.member.roles.find('name', 'Wielder')) {
+            let sub = message.content.substring(config.modprefix.length+5);
+            let args = sub.split(" ");
+            let userToKick = message.mentions.users.first();
+            var reason = sub.substring(args[0].length+1);
+            message.guild.member(userToKick).kick();
+            message.guild.channels.find('name', 'mod-log').sendMessage("**User has felt the power of Hammer!** \n**User that was kicked**: " + userToKick + "\n**Reason**: " + reason + "\n**Date**: " + today + "\n**Time of Action**: " + time + "\n**Responsible Wielder**: " + message.author + "\n**Channel**: " + message.channel.name);
+        }
+    }
+
+    if (message.content.startsWith(config.modprefix ; "ban")) {
+        if (message.member.roles.find('name', 'Wielder')) {
+            let sub = message.content.substring(config.modprefix.length+4);
+            let args = sub.split(" ");
+            let userToBan = message.mentions.users.first();
+            var reason = sub.substring(args[0].length+1);
+            message.guild.member(userToBan).ban();
+            message.guild.channels.find('name', 'mod-log').sendMessage("**User has felt the power of Hammer!** \n**User that was banned**: " + userToBan + "\n**Reason**: " + reason + "\n**Date**: " + today + "\n**Time of Action**: " + time + "\n**Responsible Wielder**: " + message.author);
+        }
+    }
+
+    if (message.content.startsWith(config.modprefix ; "warn")) {
+        if (message.member.roles.find('name', 'Wielder')) {
+            let sub = message.content.substring(config.modprefix.length+5);
+            let args = sub.split(" ");
+            let userToWarn = message.mentions.users.first();
+            var reason = sub.substring(args[0].length+1);
+            message.guild.member(userToWarn).sendMessage("Hello! You have been warned for: " + reason + " by " + message.author + "in" + message.guild.name);
+            message.guild.channels.find('name', 'mod-log').sendMessage("**User has felt the power of Hammer!** \n**User that was warned**: " + userToWarn + "\n**Reason**: " + reason + "\n**Date**: " + today + "\n**Time of Action**: " + time + "\n**Responsible Wielder**: " + message.author);
+        }
+}
     });
     // THIS  MUST  BE  THIS  WAY
 
